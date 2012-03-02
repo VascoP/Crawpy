a0fb7368847a0fca679330a2107ce7eef9c2a52a
VascoP <vascogpinho@gmail.com>
2012-03-02 11:23:57 +0000

    reorganize

--- crawler.py
+++ crawler.py
@@ -1,47 +1,18 @@
 #!/usr/bin/python
-from bs4 import BeautifulSoup as Soup
-from bs4 import SoupStrainer
-
-import requests as Reqs
-
-import urlparse
-
-from datetime import datetime
-
-from database import *
+from database import Database, Webpage
 
 
-seed_links = ("http://www.stackoverflow.com", "http://dir.yahoo.com/", "http://www.dmoz.org/", "http://www.reddit.com", "http://news.ycombinator.com")
-
-
-def get_page_content(link):
-   return Reqs.get(link)
-
-def extract_page_links(link, page_content):
-   only_a_tags = SoupStrainer("a", href=True)
-   a_tag_list = [(urlparse.urljoin(link, a_tag.get("href")), datetime.now()) for a_tag in Soup(page_content, parse_only=only_a_tags).find_all("a", href=True)]
-   return a_tag_list
-
-def save_page(db, response):
-   search = db.session.query(Webpage).filter(Webpage.full_url == response.url).first()
-   if search is not None:
-      search.num_crawls += 1
-   else:
-      w = Webpage(response.url, response.text)
-      db.session.add(w)
-   return db
+frontier = seed_links = ("http://www.stackoverflow.com", "http://dir.yahoo.com/", "http://www.dmoz.org/", "http://www.reddit.com", "http://news.ycombinator.com")
 
 
 if __name__ == "__main__":
    db = Database()
    
-   for link in seed_links:
-      response = get_page_content(link)
-      save_page(db, response)
+   for link in frontier:
+      page = Webpage(link)
+      page.save_page(db)
 
    db.session.commit()
-      #new_links = extract_page_links(link, page_content)
-      #save_page_links(new_links)
 
 
 
--- database.py
+++ database.py
@@ -7,6 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from urlparse import urlparse, urljoin
 
+from bs4 import BeautifulSoup as Soup
+from bs4 import SoupStrainer
+
+import requests as Reqs
+
 
 # Website
    # n / hash       int/hash
@@ -36,11 +41,13 @@ class Webpage(Base):
    full_url = Column(String)
    html = Column(String)
    num_crawls = Column(Integer, default = 1)
+   num_harvests = Column(Integer, default = 0)
    
    
-   def __init__(self, full_url, html):
-      self.full_url = full_url
-      self.html = html
+   def __init__(self, link):
+      response = self.get_page_content(link)
+      self.html = response.text
+      self.full_url = response.url
 
    def __repr__(self):
       return "<Webpage('%s')>" % (self.full_url)
@@ -49,9 +56,25 @@ class Webpage(Base):
       return urlparse(self.full_url).path
 
    def get_base_url(self):
-      return urljoin(urlparse(self.full_url).scheme, urlparse(self.full_url).netloc)   
-      
+      return urljoin(urlparse(self.full_url).scheme, urlparse(self.full_url).netloc)
 
+   def extract_page_links(self):
+      only_a_tags = SoupStrainer("a", href=True)
+      a_tag_list = [a_tag.get("href") for a_tag in Soup(html, parse_only=only_a_tags).find_all("a", href=True)]
+      return a_tag_list
+
+   def get_page_content(self, link):
+      return Reqs.get(link)
+
+   def save_page(self, db):
+      search = db.session.query(Webpage).filter(Webpage.full_url == self.full_url).first()
+      if search is not None:
+         search.num_crawls += 1
+      else:
+         db.session.add(self)
+      return db
+
+      
 
 class Database(object):
    session = None
