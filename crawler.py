fa6dab0ad49cd19159c16e2920687b7825d4e483
VascoP <vascogpinho@gmail.com>
2012-03-03 16:26:10 +0000

    ignore non crawlable links, log connection errors

--- crawler.py
+++ crawler.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python
 from database import Database, Webpage
 
+# Number of pages we crawl before commiting to the database
+# More is faster but riskier (lose every uncommited crawl on error)
+COMMIT_BATCH = 10
 
-seed_links = ["http://www.isup.me/google.com"]#, "http://www.stackoverflow.com", "http://dir.yahoo.com/", "http://www.dmoz.org/", "http://www.reddit.com", "http://news.ycombinator.com"]
-frontier = []
+# If each frontier is a level in the webpage graph
+# this is how many layers down we will craw on an execution
+MAX_DEPTH_GRAPH = 5
+
+# Selected seeds by hand
+SEED_LINKS = ["http://veja.abril.com.br/"]#"http://www.isup.me/google.com", "http://www.stackoverflow.com", "http://dir.yahoo.com/", "http://www.dmoz.org/", "http://www.reddit.com", "http://news.ycombinator.com"]
+
 
 if __name__ == "__main__":
+   frontier = []
    db = Database()
 
-   # Get the frontier
-   results = db.session.query(Webpage).filter(Webpage.num_harvests == 0)[:2]
+   for i in xrange(MAX_DEPTH_GRAPH):
+      # Get the frontier
+      results = db.session.query(Webpage).filter(Webpage.num_harvests == 0)[:2]
+      
+      # Harvest frontier for links
+      for result in results:
+         frontier += result.extract_links()
+         result.num_harvests += 1
 
-   # Harvest frontier for links
-   for result in results:
-      frontier += result.extract_links()
-      result.num_harvests += 1
+      # If there's nothing to harvest (first run, or the last frontier had no new links)
+      # we use the seed link set to start crawling
+      if not results:
+         print "No frontier found. Seeding the crawler."
+         frontier = SEED_LINKS
 
-   # If there's nothing to harvest (first run, or the last frontier had no new links)
-   # we use the seed link set to start crawling
-   if not results:
-      print "No frontier found. Seeding the crawler."
-      frontier = seed_links
-
-   # Crawl frontier
-   for (counter, link) in enumerate(frontier):
-      print "[{}/{}] Crawling {}".format(counter+1, len(frontier), link)
-      page = Webpage(link)
-      page.save_crawl(db)
-      if counter % 10 == 0:
-         db.session.commit()
-   db.session.commit()
+      # Crawl frontier
+      for (counter, link) in enumerate(frontier):
+         print "[{}/{}] Crawling {}".format(counter+1, len(frontier), link)
+         page = Webpage(link)
+         page.save_crawl(db)
+         if counter % COMMIT_BATCH == 0:
+            db.session.commit()
+      db.session.commit()
       
    
 
--- database.py
+++ database.py
@@ -2,8 +2,9 @@
 from os import path
 
 from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker, column_property
+from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, column_property
 
 from urlparse import urlparse, urljoin
 
@@ -12,9 +13,13 @@ from bs4 import SoupStrainer
 
 import requests as Reqs
 
+from datetime import date
+import logging
 
 
 Base = declarative_base()
+
+logging.basicConfig(level=logging.INFO, filename='logs/connection_errors_{}.log'.format(date.today()))
 
 
 class Webpage(Base):
@@ -25,8 +30,8 @@ class Webpage(Base):
    last_crawl_date = Column(DateTime, default=func.now(), onupdate=func.now())
 
    # Juice
-   full_url = Column(String)
-   html = Column(String)
+   full_url = Column(String(4000, convert_unicode=True))     # Max url said to be 2000 characters
+   html = Column(LONGTEXT(convert_unicode=True))
    num_crawls = Column(Integer, default = 1)
    num_harvests = Column(Integer, default = 0)
    
@@ -46,12 +51,31 @@ class Webpage(Base):
       return urljoin(urlparse(self.full_url).scheme, urlparse(self.full_url).netloc)
 
    def extract_links(self):
+      url_list = []
       only_a_tags = SoupStrainer("a", href=True)
-      url_list = [url.get("href") if urlparse(url.get("href")).scheme else urljoin(self.full_url, url.get("href")) for url in Soup(self.html, parse_only=only_a_tags).find_all("a", href=True)]
+      
+      crawlable = ("http", "https", "")
+      
+      for url in Soup(self.html, parse_only=only_a_tags).find_all("a", href=True):
+         url = url.get("href")
+         url_scheme = urlparse(url).scheme
+         if url_scheme in crawlable:
+            # we got a full url here
+            if url_scheme:
+               url_list.append(url)
+            # rebase that shit
+            else:
+               url_list.append(urljoin(self.full_url, url))
       return url_list
 
    def get_content(self, link):
-      return Reqs.get(link)
+      response = None
+      while not response:
+         try:
+            response = Reqs.get(link)
+         except Reqs.ConnectionError, e:
+            logging.exception(e)
+      return response
 
    def save_crawl(self, db):
       result = db.session.query(Webpage).filter(Webpage.full_url == self.full_url).first()
@@ -66,9 +90,10 @@ class Webpage(Base):
 class Database(object):
    session = None
 
-   def __init__(self, db="sqlite:///db.sqlite"):
+   #sqlite:///db.sqlite
+   def __init__(self, db="mysql://root:@localhost/crawlerdb"):
       # Engine and schema creation
-      self.engine = create_engine(db)
+      self.engine = create_engine(db, encoding="utf8")
       Base.metadata.create_all(self.engine)
       # Get a session so we can do stuff
       Session = sessionmaker(bind=self.engine)
